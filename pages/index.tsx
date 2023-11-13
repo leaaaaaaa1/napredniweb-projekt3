7ffd89c23790511b85dfeb502c50960b21df915e
Pusiclea <lp53593@fer.hr>
2023-11-13 14:30:07 +0100

    Initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,340 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import React, { useState, useEffect, useRef } from 'react';
+import styles from '../styles/asteroids.module.css'; 
 
-const inter = Inter({ subsets: ['latin'] })
+interface IAsteroid {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  velocity: {
+    x: number;
+    y: number;
+  };
+}
 
-export default function Home() {
+interface IPlayer {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+}
+
+interface IStar {
+  x: number;
+  y: number;
+  velocity: number;
+}
+
+const generateStars = (count: number, canvasWidth: number, canvasHeight: number): IStar[] => {
+  const stars: IStar[] = [];
+  for (let i = 0; i < count; i++) {
+    stars.push({
+      x: Math.random() * canvasWidth,
+      y: Math.random() * canvasHeight,
+      velocity: Math.random() * 2 + 1, // Random velocity for each star
+    });
+  }
+  return stars;
+};
+
+export default function Asteroids() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [stars, setStars] = useState<IStar[]>([]);
+  const directionRef = useRef({ x: 0, y: 0 });
+  const [asteroids, setAsteroids] = useState<IAsteroid[]>([]);
+  const [player, setPlayer] = useState<IPlayer>({ x: 0, y: 0, width: 30, height: 30 });
+  const [currentTime, setCurrentTime] = useState(0);
+  const [bestTime, setBestTime] = useState(0);
+  const spaceshipImage = useRef<HTMLImageElement | null>(null);
+  const asteroidImage = useRef<HTMLImageElement | null>(null);
+  const [rotation, setRotation] = useState(0); // okretanje slike po tipkama
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = '/spaceship.png'; //učitala posebnu sliku za igrača
+    spaceshipImage.current = img;
+    const asteroidImg = new Image();
+    asteroidImg.src = '/asteroid.png'; // Adjust this path to your asteroid image
+    asteroidImage.current = asteroidImg;
+    const canvas = canvasRef.current;
+    if (canvas) {
+      const canvasWidth = window.innerWidth;
+      const canvasHeight = window.innerHeight;
+      setStars(generateStars(100, canvasWidth, canvasHeight)); // Generate 100 stars
+    }
+    const handleUnload = () => {
+      localStorage.removeItem('bestTime'); 
+    };
+  
+    const savedBestTime = Number(localStorage.getItem('bestTime')) || 0;
+    setBestTime(savedBestTime);
+    window.addEventListener('beforeunload', handleUnload);
+  
+    return () => {
+      window.removeEventListener('beforeunload', handleUnload);
+    };
+  }, []);
+
+  const handleKeyDown = (event: KeyboardEvent) => {
+    switch (event.key) {
+      case 'ArrowLeft':
+        setRotation(-90);
+        directionRef.current = { x: -5, y: 0 };
+        break;
+      case 'ArrowRight':
+        setRotation(90);
+        directionRef.current = { x: 5, y: 0 };
+        break;
+      case 'ArrowUp':
+        setRotation(0);
+        directionRef.current = { x: 0, y: -5 };
+        break;
+      case 'ArrowDown':
+        setRotation(180);
+        directionRef.current = { x: 0, y: 5 };
+        break;
+    }
+  };
+
+  const handleKeyUp = () => {
+    directionRef.current = { x: 0, y: 0 };
+  };
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    window.addEventListener('keyup', handleKeyUp);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+      window.removeEventListener('keyup', handleKeyUp);
+    };
+  }, []);
+
+  const generateOffscreenAsteroid = (): IAsteroid => {
+    const side = Math.floor(Math.random() * 4); 
+    let x = 0, y = 0;
+    let velocityX = 0, velocityY = 0;
+  
+    const speed = Math.random() * 8 + 1; // brzina
+  
+    switch(side) {
+      case 0: // npr da ulaze u scenu s lijeve strane, itd..
+        x = -60;
+        y = Math.random() * window.innerHeight;
+        velocityX = speed;
+        break;
+      case 1:
+        x = Math.random() * window.innerWidth;
+        y = -60;
+        velocityY = speed;
+        break;
+      case 2: 
+        x = window.innerWidth + 60;
+        y = Math.random() * window.innerHeight;
+        velocityX = -speed;
+        break;
+      case 3: 
+        x = Math.random() * window.innerWidth;
+        y = window.innerHeight + 60;
+        velocityY = -speed;
+        break;
+    }
+  
+    return {
+      x,
+      y,
+      width: 40,
+      height: 40,
+      velocity: { x: velocityX, y: velocityY },
+    };
+  };
+  
+
+  const createInitialAsteroids = (count: number): IAsteroid[] => {
+    return Array.from({ length: count }, generateOffscreenAsteroid);
+  };
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (canvas) {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+      const ctx = canvas.getContext('2d');
+      if (ctx) {
+        setPlayer({
+          x: canvas.width / 2 - 15,
+          y: canvas.height / 2 - 15,
+          width: 30,
+          height: 30
+        });
+
+        setAsteroids(createInitialAsteroids(5));
+      }
+    }
+
+    const asteroidInterval = setInterval(() => {
+      setAsteroids(prevAsteroids => [...prevAsteroids, generateOffscreenAsteroid()]);
+    }, 10000); //svakih 10 sekundi se dodaje novi asteroid
+
+    return () => clearInterval(asteroidInterval);
+  }, []);
+
+  const [gameOver, setGameOver] = useState(false);
+
+  const checkCollision = (player: IPlayer, asteroid: IAsteroid) => {
+    const scaleFactor = 2; // samo da mogu malo povećati sliku
+    const scaleFactor2 = 1.5;
+    let playerRadius = 0;
+    let asteroidRadius = 0;
+    if(rotation === 180 || rotation === 90) {
+      playerRadius = Math.min(player.width, player.height) * scaleFactor / 3;
+      asteroidRadius = Math.min(asteroid.width, asteroid.height) * scaleFactor2 / 3;
+    } else {
+      playerRadius = Math.min(player.width, player.height) * scaleFactor / 2;
+      asteroidRadius = Math.min(asteroid.width, asteroid.height) * scaleFactor2 / 2;
+    }
+  
+    const dx = player.x - asteroid.x;
+    const dy = player.y - asteroid.y;
+    const distance = Math.sqrt(dx * dx + dy * dy);
+  
+    return distance < (playerRadius + asteroidRadius);
+  };  
+  const restartGame = () => {
+    setAsteroids([]); 
+    setPlayer({ x: window.innerWidth / 2, y: window.innerHeight / 2, width: 30, height: 30 });
+    setGameOver(false);
+    setAsteroids(createInitialAsteroids(5)); 
+    setCurrentTime(0);
+  };
+
+  useEffect(() => {
+    const gameLoop = () => {
+      if (gameOver) return;
+  
+      const direction = directionRef.current;
+      setPlayer(prevPlayer => ({
+        ...prevPlayer,
+        x: (prevPlayer.x + direction.x + window.innerWidth) % window.innerWidth,
+        y: (prevPlayer.y + direction.y + window.innerHeight) % window.innerHeight,
+      }));
+  
+      setAsteroids(prevAsteroids =>
+        prevAsteroids.map(asteroid => {
+          let newX = asteroid.x + asteroid.velocity.x;
+          let newY = asteroid.y + asteroid.velocity.y;
+  
+          // Wrap around the canvas
+          if (newX < -asteroid.width) {
+            newX = window.innerWidth;
+          } else if (newX > window.innerWidth) {
+            newX = -asteroid.width;
+          }
+  
+          if (newY < -asteroid.height) {
+            newY = window.innerHeight;
+          } else if (newY > window.innerHeight) {
+            newY = -asteroid.height;
+          }
+  
+          if (checkCollision(player, asteroid)) {
+            setGameOver(true);
+            const adjustedTime = currentTime + 100;
+            if (currentTime > bestTime) {
+              setBestTime(adjustedTime);
+              localStorage.setItem('bestTime', adjustedTime.toString());
+            }
+          }
+          return {
+            ...asteroid,
+            x: newX,
+            y: newY,
+          };
+        })
+      );
+      if (!gameOver) {
+        setCurrentTime((prevTime) => prevTime + 100);
+      }
+    };
+  
+    const id = setInterval(gameLoop, 100);
+    return () => clearInterval(id);
+  }, [player, gameOver, currentTime, bestTime]);
+  
+
+  useEffect(() => {
+    const render = () => {
+      const canvas = canvasRef.current;
+      if (canvas) {
+        const ctx = canvas.getContext('2d');
+        if (ctx) {
+          ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+          // Draw stars
+          ctx.fillStyle = 'white';
+          stars.forEach(star => {
+            ctx.beginPath();
+            ctx.arc(star.x, star.y, 1, 0, Math.PI * 2);
+            ctx.fill();
+          });
+
+          ctx.save();
+          ctx.translate(player.x + player.width / 2, player.y + player.height / 2);
+          ctx.rotate((rotation * Math.PI) / 180);
+          const scaleFactor = 2; // samo da mogu malo povećati sliku
+          const scaleFactor2 = 1.5;
+          if(spaceshipImage.current) {
+            ctx.drawImage(spaceshipImage.current, -player.width, -player.height, player.width * scaleFactor, player.height * scaleFactor);
+          }
+          ctx.restore();
+          asteroids.forEach(asteroid => {
+            if(asteroidImage.current) {
+                ctx.drawImage(asteroidImage.current, asteroid.x, asteroid.y, asteroid.width * scaleFactor2, asteroid.height * scaleFactor2);
+            }
+          });
+
+          requestAnimationFrame(render);
+        }
+      }
+    };
+
+    render();
+  }, [player, asteroids, rotation]);
+
+  const formatTime = (time: number): string => {
+    const minutes = Math.floor(time / 60000);
+    const seconds = Math.floor((time % 60000) / 1000);
+    const milliseconds = time % 1000;
+    return `${minutes}:${seconds.toString().padStart(2, '0')}.${milliseconds.toString().padStart(3, '0')}`;
+  };
+
+  useEffect(() => {
+    const updateStars = () => {
+      setStars(prevStars => 
+        prevStars.map(star => ({
+          ...star,
+          y: (star.y + star.velocity) % window.innerHeight, // Update each star's position
+        }))
+      );
+      requestAnimationFrame(updateStars);
+    };
+    requestAnimationFrame(updateStars);
+  }, []);
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className={styles.container}>
+      <canvas ref={canvasRef} className={styles.gameCanvas}></canvas>
+      <div className={styles.timer}>Time: {formatTime(currentTime)}</div>
+      {gameOver && (
+        <div className={styles.overlay}>
+          <div className={styles.form}>
+            <h1 className={styles.heading}>Game Over</h1>
+            <p className={styles.heading}>Your Time: {formatTime(currentTime)}</p>
+            <div className={styles.heading}>Best Time: {formatTime(bestTime)}</div>
+            <button className={styles.submitBtn} onClick={restartGame}>Restart Game</button>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      )}
+    </div>
+  );
 }
